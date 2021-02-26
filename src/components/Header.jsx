99b91ecc8acbe45e5e13e3f5b68f3dbde79ebb94
luisangel1808 <luisangel1808@hotmail.com>
2021-02-26 08:38:08 -0500

    Initial commit, basic folders structure, posible routes, Initial State.js for emulate products

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1 className="Header-title">Corebo Store</h1>
+      <div className="Header-checkout">
+          Checkout
+      </div>
+    </div>
+  );
+}
+
+export default Header;
