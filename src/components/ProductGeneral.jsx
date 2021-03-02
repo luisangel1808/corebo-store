687c397bbc71e8dad73e042f3e2d5a5c6d6d74b4
luisangel1808 <luisangel1808@hotmail.com>
2021-03-02 10:37:31 -0500

    actualización initialState y otros cambios menores

---
+++ src/components/ProductGeneral.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const ProductGeneral = ({ product, handleAddToCart }) => {
+  return (
+    <div className="Product-item">
+      <img src={product.image[0]} alt={product.title} />
+      <div className="Product-item-info">
+      <Link to={`productMain/${product.id}`}>
+        <h2>
+          {product.title}
+          <span>$ {product.price}</span>
+        </h2>
+        </Link>
+        <p>{product.description}</p>
+      </div>
+      <button type="button" onClick={handleAddToCart(product)}>
+        Comprar
+      </button>
+    </div>
+  );
+};
+
+export default ProductGeneral;
