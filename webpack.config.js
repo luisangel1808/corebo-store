0d652cc5e35b770e470b58512228bae76a36ddaa
luisangel1808 <luisangel1808@hotmail.com>
2021-03-03 07:50:53 -0500

    Aplicada la configuración de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+const TersetJSPlugin = require('terser-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: path.resolve(__dirname, 'src', 'index.js'),
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/',
+    filename: 'js/[name].[fullhash].js',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -40,20 +44,27 @@ module.exports = {
       },
     ],
   },
-
+  optimization: {
+    minimizer: [new TersetJSPlugin()],
+  },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-    }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].css',
+      filename: 'css/[name].[fullhash].css',
+      chunkFilename: 'css/[id].[fullhash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+    new webpack.DllReferencePlugin({
+      manifest: require('./modules.manifest.json'),
+    }),
+    new AddAssetHtmlPlugin({
+      filepath: path.resolve(__dirname, 'dist/js/*.dll.js'),
+      outputPath: 'js',
+      publicPath: './js/',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/app.*'],
     }),
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    historyApiFallback: true,
-    port: 3005,
-  },
 };
